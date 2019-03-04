7af02750011c1186a038bf42d5740c46a962f5e1
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-04 15:21:42 +0100

    form with images

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,4 +2,5 @@ class Experience < ApplicationRecord
   validates :name, :photo, :user, :price_per_day, :description, presence: true
   belongs_to :user
   has_many :bookings
+  mount_uploader :photo, PhotoUploader
 end

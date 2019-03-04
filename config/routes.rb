b815773fc2ddeba408f2e822eadc788de1ae3a39
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-04 16:02:42 +0100

    banner

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :experiences
   resources :bookings
+  get '/my_experiences' => 'experiences#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

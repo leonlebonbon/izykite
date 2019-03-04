33911b51695b8f62a7f8cfcdb9bfb1953c73505d
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-04 14:36:51 +0100

    create  the new experience view

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,21 @@
 class ExperiencesController < ApplicationController
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @experience = Experience.new(params_experience)
+    @experience.user = current_user
+    if @experience.save
+      redirect_to(root_path)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_experience
+    params.require(:experience).permit(:address, :photo, :price_per_day, :name, :minimum_length, :description)
+  end
 end

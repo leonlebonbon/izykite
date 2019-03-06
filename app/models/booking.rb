dd9483480f186869b29ee48c32be9060b5127c99
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-06 15:42:32 +0100

    jsp

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  validates :number_of_kiters, :start_date, :end_date, presence: true
   belongs_to :experience
   belongs_to :user
+  validates :number_of_kiters, :start_date, :end_date, presence: true
 end

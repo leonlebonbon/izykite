c69a4d6f89dd2af0d238741078776deebca47b90
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-06 14:59:18 +0100

    banner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @experiences = policy_scope(Experience)
+    @experiences = policy_scope(Experience)[0..5]
   end
 end

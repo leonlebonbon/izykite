2064e91d68e0e84205dc2713e02c2b53dcb03cbb
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-06 16:22:31 +0100

    start and end and review new

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
-  belongs_to :user, through: :booking
-  belongs_to :experience, through: :booking
-  validates :rating, presence: :true, inclusion: { in: (0..5).to_a }
+  belongs_to :user
+  belongs_to :experience
+  validates :rating, presence: true, inclusion: { in: (0..5).to_a }
 end

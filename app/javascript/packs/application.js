76c35018887982991a53daeed08816adb109e5b4
Leonie Mironesco <leonie.mironesco@hec.edu>
2019-03-07 14:57:53 +0100

    pull

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,9 @@ import "bootstrap";
 import "../plugins/flatpickr";
 // import "../search/search";
 // import "../search/xp-address";
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '../form/booking.js'
+
 import { initMapbox } from '../plugins/init_mapbox';
 
 import { initAutocomplete } from "../plugins/init_autocomplete"

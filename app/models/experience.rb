fa4dd7b0384200efc20dc170be8b60bfb0fc2bd0
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-07 18:20:34 +0100

    sauvées

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,5 @@
 class Experience < ApplicationRecord
-  validates :name, :user, :price_per_day, :description, presence: true
+  validates :name, :user, :price_per_day, :description, :photo, :address, :minimum_stay, presence: true
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
